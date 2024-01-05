1f38439ae1a9af70d6779dcc649b72f5c2e30e32
David Emaye <emayeodavid@gmail.com>
2024-01-05 18:36:59 +0100

    created nuxt app and addded some folders

---
+++ layouts/auth-layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="form-ctn">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'auth-layout'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="global_ctn">
+    <DashboardHeader @open-modal="showLogout = true" />
+    <div class="sidebar_main">
+      <DashboardSideBar />
+      <div class="main_content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const showLogout = ref(false)
+</script>
+
+<style scoped>
+.global_ctn {
+  background-color: #fff;
+}
+.sidebar_main {
+  display: flex;
+}
+
+.main_content {
+  background-color: #fff;
+  padding: 3rem 2vw;
+  margin-top: 90px;
+  margin-left: 17vw;
+  /* z-index: 1; */
+  width: 83%;
+  max-width: 1300px;
+}
+</style>
